13331b277059542e1ba09698834a9d63f6c52213
takasaki kohei <takasaki@zyyx.jp>
2020-08-02 00:57:55 +0900

    chang div to h1

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
+    <h1 className="App">
+      Hello World!
+    </h1>
   );
 }
 
