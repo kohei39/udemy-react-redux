bc910cf71d9af7f77befb0c9b23b3fbdee09b62b
takasaki kohei <takasaki@zyyx.jp>
2020-08-17 20:44:01 +0900

    create class component and functional component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import React from 'react';
 
-function App() {
-  // const greeting = 'Hi, Tom!';
-  // const dom = <h1 className="App">{greeting}</h1>
+// function App() {
+//   // const greeting = 'Hi, Tom!';
+//   // const dom = <h1 className="App">{greeting}</h1>
+//   return (
+//     <React.Fragment>
+//       <label htmlFor="bar">bar</label>
+//       <input type="text" onChange={() => {console.log('I am clicked')}} />
+//     </React.Fragment>
+//   );
+// }
+
+const App = () => {
   return (
-    <React.Fragment>
-      <label htmlFor="bar">bar</label>
-      <input type="text" onChange={() => {console.log('I am clicked')}} />
-    </React.Fragment>
-  );
+    <div>
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
+    </div>
+  )
 }
 
+const Cat = () => {
+  return <div>Meow!</div>
+}
 export default App;
