aa62762333908d2d45b7d9ee0c9aac8a89884ccf
takasaki kohei <takasaki@zyyx.jp>
2020-09-05 11:23:45 +0900

    implement delete feature

--- src/components/events_index.js
+++ src/components/events_index.js
@@ -20,14 +20,17 @@ import { Link } from 'react-router-dom'
 
 class EventsIndex extends Component {
   componentDidMount() {
-    console.log('hi')
     this.props.readEvents()
   }
   renderEvents() {
     return _.map(this.props.events, event => (
       <tr key={event.id}>
         <td>{event.id}</td>
-        <td>{event.title}</td>
+        <td>
+          <Link to={`/events/${event.id}`}>
+            {event.title}
+          </Link>
+        </td>
         <td>{event.body}</td>
       </tr>
     ))
